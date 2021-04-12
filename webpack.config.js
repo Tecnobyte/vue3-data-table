afed339608014ec80b38be278597c256a7c17b97
gerardo <gerardochale01@gmail.com>
2021-04-12 11:50:39 -0500

    primer commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require("vue-loader");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  resolve: {
+    alias: {
+      // this isn't technically needed, since the default `vue` entry for bundlers
+      // is a simple `export * from '@vue/runtime-dom`. However having this
+      // extra re-export somehow causes webpack to always invalidate the module
+      // on the first HMR update and causes the page to reload.
+      vue: "@vue/runtime-dom"
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.vue$/, use: 'vue-loader' },
+      { test: /\.css$/, use: [
+        {
+          loader: MiniCssExtractPlugin.loader
+        },
+        "css-loader"
+      ]},
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new VueLoaderPlugin(),
+  ],
+  devServer: {
+    inline: true,
+    hot: true,
+    stats: "minimal",
+    // contentBase: __dirname,
+    overlay: true,
+    injectClient: false,
+    disableHostCheck: true
+  }
+};
