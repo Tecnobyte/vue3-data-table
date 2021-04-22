881a9bce3b9803a1b64b94da9ebaa8656700c4e3
alanbaldi <alanortizbaldizon@hotmail.com>
2021-04-22 16:31:26 -0500

    upgrade server table

--- test/Index.vue
+++ test/Index.vue
@@ -1,98 +1,17 @@
 <template>
-    <h1>client table</h1>
-    <client-table :columns="column" :data="data" :options="option">
-        <template v-slot:menu="row">
-            <div> 
-                <button @click="pais(row)">seleccion</button>
-            </div>
-        </template>
-    </client-table>
+    <index-client-table v-if="false" />
+    <index-server-table v-if="true" />
 </template>
-
 <script>
-import { ClientTable } from './../src/main';
-    export default {
-        data() {
-            return {
-                column: [
-                    {
-                        description: 'id',
-                        filter: false,
-                        order: false
-                    },
-                    {
-                        description: 'descripcion',
-                        filter: true,
-                        order: false
-                    },
-                    {
-                        description: 'origen',
-                        filter: true,
-                        order: false
-                    },
-                    {
-                        description: 'poblacion',
-                        filter: false,
-                        order: false
-                    },
-                    {
-                        description: 'menu',
-                        filter: false,
-                        order: false
-                    }
-                ], data: [
-                    {
-                        id: 1,
-                        descripcion: 'mexico',
-                        origen: 'pais',
-                        poblacion: 100000,
-                    },{
-                        id: 2,
-                        descripcion: 'estados unidos',
-                        origen: 'pais',
-                        poblacion: 200000
-                    },{
-                        id: 3,
-                        descripcion: 'españa',
-                        origen: 'pais',
-                        poblacion: 300000
-                    },{
-                        id: 4,
-                        descripcion: 'canada',
-                        origen: 'pais',
-                        poblacion: 400000
-                    },{
-                        id: 5,
-                        descripcion: 'londres',
-                        origen: 'pais',
-                        poblacion: 500000
-                    },{
-                        id: 6,
-                        descripcion: 'china',
-                        origen: 'pais',
-                        poblacion: 600000
-                    }
-                ], option:{
-                    time: 500
-
-                },
-                seleccion: null,
-            };
-        },
-        components: {
-            ClientTable
-        },
-        setup() {
-        }, mounted() {
-
-        }, methods: {
-            pais(row){
-                console.log(row)
-            }
-        }
-    };
+import IndexClientTable from './index-client-table.vue';
+import IndexServerTable from './index-server-table.vue';
+export default {
+    components:{
+        IndexClientTable,
+        IndexServerTable
+    },
+    setup() {
+        
+    },
+}
 </script>
-
-<style scoped>
-
-</style>
---
+++ test/index-server-table.vue
@@ -0,0 +1,55 @@
+<template>
+    <h1>Server table</h1>
+    <server-table :columns="columns" :url="url" @loaded="loaded">
+        
+    </server-table>
+</template>
+
+<script>
+import { ServerTable } from './../src/main';
+import { ref } from 'vue'
+
+export default {
+    components: {
+        ServerTable
+    },
+    setup() {
+        let data = ref([]);
+        let url = 'http://test-server-table.test/api/get-personas';
+        let columns = [
+            {
+                description: 'id',
+                filter: false,
+                header:'ID',
+                order: false,
+            },
+            {
+                description: 'name',
+                filter: true,
+                header:'Nombre',
+                order: false,
+            },
+            {
+                description: 'last_name',
+                filter: true,
+                order: false
+            },
+            {
+                description: 'number',
+                filter: true,
+                order: false
+            },
+        ];
+
+        return {
+            columns,
+            url
+        };
+
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
