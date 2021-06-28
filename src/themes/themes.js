6f59c52263aedb23fa7679eabc48f6f798db547e
tecnobyte <gerardochale01@gmail.com>
2021-06-28 12:09:19 -0500

    modificacion estilo client-table

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -4,7 +4,23 @@ export const Themes = {
         input:'input',
         pagination:'pagination'
     },
-    ligth_theme:{
+    default_theme:{
+        table:{
+            main: 'default-table',
+            thead: 'default-thead-table',
+            tbody: 'default-tbody-table',
+        },
+        pagination:{
+        }
+    },
+    ligth:{
+        table:{
+            main: 'light-table-theme',
+            thead: 'light-thead-theme',
+            tbody: 'light-tbody-theme',
+        },
+        pagination:{
+        }
 
     }
 }
--- test/Index.vue
+++ test/Index.vue
@@ -1,17 +1,14 @@
 <template>
-    {{ server }}
     <index-client-table v-if="client" />
     <index-server-table v-else >
     </index-server-table>
-
-    <hola />
 </template>
 <script>
 
 import IndexClientTable from './index-client-table.vue';
 import IndexServerTable from './index-server-table.vue';
-
 import { config } from './config.js';
+
 export default {
     components:{
         IndexClientTable,
